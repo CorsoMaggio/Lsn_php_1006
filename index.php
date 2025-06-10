7fbb37883072543e5b6fccc505763a84d25520ca
Leysan@LAPTOP-25B9S5T6 <leisan.nasretdinova@gmail.com>
2025-06-10 10:44:26 +0200

    ho provato

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$age = 28;
+const MAX = 18;
+if ($age >= MAX) {
+    echo 'Sono maggiorenne e ho piu di 18 anni';
+} else if  ($age < MAX){
+    echo 'Sono minorenne';
+} else {
+    echo 'Sono maggiorenne e ho esattamente 18 anni';
+}
+?>
+
+
+
